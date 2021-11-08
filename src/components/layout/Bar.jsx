00e731c417753d8d39650e282ce7efd677c730bd
MrRedPanda <jorgeloredohdz@gmail.com>
2021-11-08 17:03:37 -0600

    line comment deleted

--- src/components/layout/Bar.jsx
+++ src/components/layout/Bar.jsx
@@ -5,7 +5,6 @@ const Bar = () => {
         <header className="app-header">
             <p className="nombre-usuario">Hola <span>Pepito</span></p>
             <nav className="nav-principal">
-                {/* <a href="#!"><i class="fas fa-sign-out-alt"></i> Cerrar Sesión</a> */}
                 <button class="btn btn-block btn-logout"><i class="fas fa-sign-out-alt logout-red"></i> Cerrar Sesión</button>
             </nav>
         </header>
