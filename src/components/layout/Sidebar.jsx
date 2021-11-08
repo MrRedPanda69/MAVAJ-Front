bdd39a3fee9104e39e048b9d0053a31b4565dcf6
MrRedPanda <jorgeloredohdz@gmail.com>
2021-11-08 17:00:39 -0600

    more styles added

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -5,8 +5,8 @@ import ListProyects from '../proyectos/ListProyects';
 const Sidebar = () => {
     return (  
         <aside>
-            <h1 className="ft-sidebar">MAVAJ<span>Tool</span></h1>
-            <NewProyect></NewProyect>
+            <h1 className="ft-sidebar">MAVAJ<span> Tool</span></h1>
+            <NewProyect/>
 
             <div className="proyectos">
                 <h2 className="ft-sidebar">Tus Proyectos</h2>
