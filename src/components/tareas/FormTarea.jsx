96a2684d995a17332a38a194f22bb36e5677bd37
MrRedPanda <jorgeloredohdz@gmail.com>
2021-11-08 17:05:56 -0600

    ListTareas.jsx, Tarea.jsx and FormTarea.jsx added as well as import react and stateless functional component

---
+++ src/components/tareas/FormTarea.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FormTarea = () => {
+    return (  );
+}
+ 
+export default FormTarea;
---
+++ src/components/tareas/ListTareas.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ListTareas = () => {
+    return (  );
+}
+ 
+export default ListTareas;
